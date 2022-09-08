0636b9657350059df79cd1e127d006adc09d5e8c
lwm2120 <lwm2120@rit.edu>
2022-09-07 23:11:56 -0400

    stub

--- gen.sh
+++ gen.sh
@@ -6,4 +6,14 @@ title_wrapper() {
     echo "$1" | sed -E -e "s/\..+$//g"  -e "s/_(.)/ \u\1/g" -e "s/^(.)/\u\1/g"
 }
 
+# Header gen function
+# Directory (projects/art/posts) - no 'view raw'
+# Individual post - same with breadcrumbs - w/ 'view raw'
 
+# Footer gen function
+# Individual post only - links? - 'view raw'
+
+# Main page gen
+
+# Name
+# ---
